528652ab1aa04f7c7cd1ed4fb87202c999a30024
Denis <dkozin@holmix.ru>
2021-10-11 04:16:18 +0300

    made 1st

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from products.views import index, products
+from django.urls import path, include
+from products.views import products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index.html', index, name='index'),
-    path('products.html', products, name='products'),
-    path('', index, name='index')
+    path('', include('products.urls', namespace='products')),
+    path('products/', products, 'products')
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+import json
 
 
 # Create your views here.
 
 def index(request):
+    context = {
+        'title': 'index',
+    }
     return render(request, 'products/index.html')
 
 
 def products(request):
+    with open('products/templates/products/prod.json', encoding='utf-8') as f:
+        prs = json.load(f)
+    context = {
+        'title': 'products',
+        'products': prs
+    }
     return render(request, 'products/products.html')
