a5e60423394b65ff64648110e733b51bb18feb7d
Denis <dkozin@holmix.ru>
2021-10-14 01:50:55 +0300

    changed models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Категория товара')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование товара')
+    desc = models.TextField(blank=True, verbose_name='Описание товара')
+    price = models.DecimalField(verbose_name='Цена')
+    image = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    is_published = models.BooleanField(default=True, verbose_name='Флаг публикации')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['id']
