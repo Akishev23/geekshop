661c41393e9b3887b9fde4f0ed24d60373ff46b0
Denis <dkozin@holmix.ru>
2021-10-17 03:11:36 +0300

    small corrections

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.conf.urls import url
 from .views import index, products, get_cat
 
 app_name = 'products'
@@ -7,5 +6,4 @@ app_name = 'products'
 urlpatterns = [
     path('index/', index, name='index'),
     path('', products, name='products'),
-
 ]
