52dd8eb544c11913fa015207301dca6cd8f87c9f
Denis <dkozin@holmix.ru>
2021-10-17 05:52:18 +0300

    worked with templates and own tags

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'templates', os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
