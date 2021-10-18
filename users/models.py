5655c35e7af22521d940c64c1a452ffd009119e7
Denis <dkozin@holmix.ru>
2021-10-18 23:28:10 +0300

    made register, login, logout

--- users/models.py
+++ users/models.py
@@ -6,3 +6,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='user_image', blank=True)
+
--- users/views.py
+++ users/views.py
@@ -1,9 +1,44 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from users.forms import UserLoginForm, UserRegisterForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 def login(request):
-    return render(request, 'users/login.html', {'title': 'GeeShop - Авторизация'})
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'title': 'GeeShop - Авторизация',
+                                                'form': form})
 
 
 def register(request):
-    return render(request, 'users/register.html', {'title': 'Geekshop - Регистрация'})
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'title': 'Geekshop - Регистрация',
+                                                   'form': form})
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
