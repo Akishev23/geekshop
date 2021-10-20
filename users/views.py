9684fedd4bfb129ec150180d4eb1f52064315fde
Denis <dkozin@holmix.ru>
2021-10-20 05:15:35 +0300

    made improved messages

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from django.contrib import messages
-from users.forms import UserLoginForm, UserRegisterForm
+from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -39,6 +39,20 @@ def register(request):
 
     return render(request, 'users/register.html', {'title': 'Geekshop - Регистрация',
                                                    'form': form})
+
+
+def profile(request):
+
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Данные сохранены успешно')
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            messages.error(request, 'Ошибка сохранения')
+    return render(request, 'users/profile.html', {'title': 'Личный кабинет',
+                                                  'form': UserProfileForm(instance=request.user)})
 
 
 def logout(request):
